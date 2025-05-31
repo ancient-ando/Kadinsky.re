02b7824acf5db3ef7693821cb7dd321c991c1d19
Ando <ancient.prophet@icloud.com>
2025-05-31 18:42:39 +0100

    Things look not fun now

--- infinity.lua
+++ infinity.lua
@@ -2,22 +2,25 @@ new_infinity = false
 
 function shift_infinity()
     if not new_infinity then return end
-    local c_r = ceil(sqrt(player.bubble_size) / 8) + 1.5
+    c_r = ceil(sqrt(player.bubble_size) / 8) + 2.5
+    c_r2 = c_r * c_r 
     local s_x, s_y = sgn(cshift_x), sgn(cshift_y)
-    local c_x, c_y = mid_x / 8, mid_y / 8
-    local cmin_x, cmax_x = min_x / 8, max_x / 8
-    local cmin_y, cmax_y = min_y / 8, max_y / 8
+    c_x, c_y = mid_x / 8, mid_y / 8
+    p_x, p_y = player.x / 8, player. y / 8
+    cmin_x, cmax_x = min_x / 8, max_x / 8
+    cmin_y, cmax_y = min_y / 8, max_y / 8
     --local c_r = c_x - cmin_x 
-    for i = c_x + s_x * c_r, c_x - s_x * c_r, -s_x do 
-        for j = c_y + s_y * c_r, c_y - s_y * c_r, -s_y do
+    --for i = c_x + s_x * c_r, c_x - s_x * c_r, -s_x do 
+        --for j = c_y + s_y * c_r, c_y - s_y * c_r, -s_y do
+    for i = p_x + s_x * c_r, p_x - s_x * c_r, -s_x do 
+        for j = p_y + s_y * c_r, p_y - s_y * c_r, -s_y do
     --for i = cmin_x + 1, cmax_x do 
         --for j = cmin_y + 1, cmax_y do 
-            ori_x, ori_y = (i - cshift_x) * 8, (j - cshift_y) * 8 
-            if (i - c_x) * (i - c_x) + (j - c_y) * (j - c_y) < c_r * c_r 
-                and ori_x > min_x and ori_x < max_x and ori_y > min_y and ori_y < max_y then
+            --ori_x, ori_y = (i - cshift_x) * 8, (j - cshift_y) * 8 
+            --if (i - c_x) * (i - c_x) + (j - c_y) * (j - c_y) < c_r * c_r then 
+            if (i - p_x) * (i - p_x) + (j - p_y) * (j - p_y) < c_r2 then
+            --if ori_x > min_x and ori_x < max_x and ori_y > min_y and ori_y < max_y then
                 mset(i, j, mget(i - cshift_x, j - cshift_y))
-            else
-                mset(i, j, 0)
             end
         end
     end
@@ -39,7 +42,8 @@ function update_infinity()
             new_infinity = true 
             infinite_timer = frame_timer
             shift_x, shift_y = mid_x - player.x, mid_y - player.y
-            cshift_x, cshift_y = flr(shift_x / 8 + 0.5), flr(shift_y / 8 + 0.5)
+            cshift_x, cshift_y = flr(shift_x / 64 + 0.5) * 8, flr(shift_y / 64 + 0.5) * 8
+            --cshift_x, cshift_y = flr(shift_x / 8 + 0.5), flr(shift_y / 8 + 0.5)
             shift_x, shift_y = cshift_x * 8, cshift_y * 8
             player.x += shift_x
             player.y += shift_y
--- level_7.lua
+++ level_7.lua
@@ -16,8 +16,8 @@ end
 init_pearson(0)
 
 function hash_pearson(x, y)
-    x, y = x \ 128 * 128, y \ 128 * 128
-    local data = {x % 256, x \ 256 % 256, y % 256, y \ 256 % 256}
+    --x, y = x \ 128 * 128, y \ 128 * 128
+    local data = {x % 251, y % 251}
     local h = #data
     foreach(data, function(d)
         --printh('\n data ' .. d, 'log.txt')
@@ -30,11 +30,40 @@ function try_mset(x, y, snum)
     --printh("try_mset->init :"..x.." "..y, "log.txt")
     --printh("c ".. cmin_x.. " ".. cmax_x .. " " .. cmin_y .. " " .. cmax_y, "log.txt")
     if x <= cmin_x or x >= cmax_x or y <= cmin_y or y >= cmax_y then return false end
-    if (x - c_x) * (x - c_x) + (y - c_y) * (y - c_y) < c_r2 then return false end
+    --if (x - c_x) * (x - c_x) + (y - c_y) * (y - c_y) < c_r2 then return false end
+    if (x - p_x) * (x - p_x) + (y - p_y) * (y - p_y) < c_r2 then return false end
     local f = fget(mget(x, y))
     --printh("f " .. f, "log.txt")
-    if 0 != f and f <= fget(snum) then return false end 
+    if 0 != f and f < fget(snum) then return false end 
     --printh("try_mset->succeed :"..x.." "..y .." "..snum, "log.txt")
+    local s = mget(x, y)
+    if f == fget(snum) then
+        if 101 == snum or (s >= 112 and s <= 115) then
+            snum = 116 == snum and 26 or snum
+            snum = 117 == snum and 25 or snum
+        end
+        if s >= 96 and s <= 99 then 
+            snum = 85 == snum and 41 or snum
+            snum = 84 == snum and 42 or snum
+        end 
+        if s >= 84 and s <= 85 and 1 == bxor(s, snum) then
+            snum = 27
+        end 
+        if s >= 116 and s <= 117 and 1 == bxor(s, snum) then
+            snum = 28
+        end 
+        if s < 84 or s > 85 then 
+            snum = 85 == snum and 79 or snum
+            snum = 84 == snum and 95 or snum
+        end 
+        snum = 117 == snum and 127 or snum
+        snum = 116 == snum and 111 or snum
+        snum = 96 == snum and 10 or snum
+        snum = 98 == snum and 12 or snum
+        snum = 97 == snum and 11 or snum
+        snum = 99 == snum and 13 or snum
+
+    end
     mset(x, y, snum)
     return true 
 end
@@ -44,13 +73,10 @@ function dream_infinity()
     if not new_infinity then return end
     c_r = ceil(sqrt(player.bubble_size) / 8) + 0.5
     c_r2 = c_r * c_r
-    c_x, c_y = mid_x / 8, mid_y / 8
-    cmin_x, cmax_x = min_x / 8, max_x / 8
-    cmin_y, cmax_y = min_y / 8, max_y / 8
 
     for i = cmin_x + 1, cmax_x - 1 do
         for j = cmin_y + 1, cmax_y - 1 do
-            if (i - c_x) * (i - c_x) + (j - c_y) * (j - c_y) >= c_r * c_r then
+            if (i - p_x) * (i - p_x) + (j - p_y) * (j - p_y) >= c_r2 then
                 mset(i, j, 0)
             end
         end
@@ -60,54 +86,59 @@ function dream_infinity()
     ----Cloud Generation-----
 
     --(deterministic first)--
-    seed = hash_pearson(tshift_x, tshift_y)
-    srand(seed)
+    --seed = hash_pearson(tshift_x, tshift_y)
+    
     --------[Cumulus]--------
-    num_cloud = 2
-    for i = 1, num_cloud do 
-        local h, x, y = flr(rnd(1) + 2), flr(rnd(24) + 4), flr(rnd(24) + 4)
-        local cloud_x, cloud_y = x, y
-        local l, r = {}, {}
-        l[y], r[y] = x, x + flr(rnd(h / 2)) + 1
-        try_mset(l[y] - 16 + c_x, y - 16 + c_y, 85)
-        try_mset(r[y] - 16 + c_x, y - 16 + c_y, 84)
-        for k = l[y] + 1, r[y] - 1 do
-            try_mset(k - 16 + c_x, y - 16 + c_y, 98)
-        end
-        for j = 1, h do 
-            y += 1
-            l[y] = l[y - 1]
-            r[y] = r[y - 1]
-            for k = l[y], r[y] do 
-                try_mset(k - 16 + c_x, y - 16 + c_y, 100)
-            end
-            if rnd(2) < 1 then
-                l[y] -= 1
-                try_mset(l[y] - 16 + c_x, y - 16 + c_y, 98)
-            end
-            if rnd(2) < 1 then 
-                r[y] += 1
-                try_mset(r[y] - 16 + c_x, y - 16 + c_y, 98)
+    hash_limit = 20
+    for a = min_x, max_x, 16 do
+        for b = min_y, max_y, 16 do
+            seed = hash_pearson(a - tshift_x, b - tshift_y)
+            srand(seed)
+            if seed <= hash_limit then 
+                local h, x, y = flr(rnd(3)), a \ 8, b \ 8 
+                local l, r = {}, {}
+                l[y], r[y] = x, x + flr(rnd(h / 2)) + 1
+                try_mset(l[y], y, 85)
+                try_mset(r[y], y, 84)
+                for k = l[y] + 1, r[y] - 1 do
+                    try_mset(k, y, 98)
+                end
+                for j = 1, h do 
+                    y += 1
+                    l[y] = l[y - 1]
+                    r[y] = r[y - 1]
+                    for k = l[y], r[y] do 
+                        try_mset(k, y, 100)
+                    end
+                    if rnd(2) < 1 then
+                        l[y] -= 1
+                        try_mset(l[y], y, 98)
+                    end
+                    if rnd(2) < 1 then 
+                        r[y] += 1
+                        try_mset(r[y], y, 98)
+                    end 
+                    l[y] -= 1
+                    r[y] += 1
+                    try_mset(l[y], y, 85)
+                    try_mset(r[y], y, 84)
+                end
+                y += 1
+                l[y] = l[y - 1]
+                r[y] = r[y - 1]
+                try_mset(l[y], y, 117)
+                try_mset(r[y], y, 116)
+                for k = l[y] + 1, r[y] - 1 do
+                    try_mset(k, y, 114)
+                end
             end 
-            l[y] -= 1
-            r[y] += 1
-            try_mset(l[y] - 16 + c_x, y - 16 + c_y, 85)
-            try_mset(r[y] - 16 + c_x, y - 16 + c_y, 84)
-        end
-        y += 1
-        l[y] = l[y - 1]
-        r[y] = r[y - 1]
-        try_mset(l[y] - 16 + c_x, y - 16 + c_y, 117)
-        try_mset(r[y] - 16 + c_x, y - 16 + c_y, 116)
-        for k = l[y] + 1, r[y] - 1 do
-            try_mset(k - 16 + c_x, y - 16 + c_y, 114)
-        end
+        end   
     end
 
     -------------------------
     for i = cmin_x + 1, cmax_x - 1 do
         for j = cmin_y + 1, cmax_y - 1 do
-            if (i - c_x) * (i - c_x) + (j - c_y) * (j - c_y) >= c_r * c_r then
+            if (i - p_x) * (i - p_x) + (j - p_y) * (j - p_y) >= c_r2 then
                 --------------------------
                 ----Terrain Generation----
                 if (c_y - j) + tshift_y / 8 < -10 then 
